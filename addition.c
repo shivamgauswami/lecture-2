f96e5df14048e26342e4e69668adbabdf030e07f
shivamgauswami <130463011+shivamgauswami@users.noreply.github.com>
2023-04-11 17:16:15 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+{
+	int a,b,c;
+	a=17;
+	b=13;
+	c=a+b;
+	printf("addition of %d & %d=%d\n",a,b,c);
+	c=a-b;
+    printf("substraction of %d &%d=%d\n",a,b,c);
+    c=a/b;
+    printf("divison of %d &%d=%d\n",a,b,c);
+    c=a*b;
+    printf("multiplication of %d &%d=%d\n",a,b,c);
+    c=a%b;
+    printf("moduler of %d &%d=%d",a,b,c);
+}
+A
